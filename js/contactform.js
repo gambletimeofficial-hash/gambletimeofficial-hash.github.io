f83bb17cc18fb861df2dc0aa3cbddef1acfe7237
gambletimeofficial-hash <gambletimeofficial@gmail.com>
2026-01-02 18:00:53 +0100

    Update contactform.js

--- js/contactform.js
+++ js/contactform.js
@@ -2,20 +2,21 @@
   emailjs.init("nsNu7SPEt8L5RsUEy"); // from EmailJS dashboard
 })();
 
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+const form = document.getElementById("contact-form");
 
-    emailjs
-      .sendForm("service_qb0hd3a", "template_dflhdyl", this)
-      .then(
-        function () {
-          alert("Contact form has been send.");
-        },
-        function (error) {
-          alert("Failed to send contact form. Please try again later.");
-          console.error(error);
-        }
-      );
-  });
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  emailjs
+    .sendForm("service_qb0hd3a", "template_dflhdyl", form)
+    .then(
+      function () {
+        alert("Contact form has been send.");
+        form.reset(); // ✅ ALWAYS clears inputs
+      },
+      function (error) {
+        alert("Failed to send contact form. Please try again later.");
+        console.error(error);
+      }
+    );
+});
